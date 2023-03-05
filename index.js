131494c4377ce69a41305ca480c931ed87ecc9b0
DaniDela253 <danidavis321@gmail.com>
2023-03-05 15:58:48 -0600

    add test & actual functionality for embedded message to new members

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ require("dotenv/config");
 
 //look at the docs for ALLLL issues!!
 
+//* All of our important stuff:
+const GuildIDs = {
+	DaniDeliaServer: "1078490032324759642",
+	Server: "1048059530711404615",
+	NVO: "938105437180551168",
+};
+
+const ChannelTags = {
+	NVOrules: "<#938916275437064213>",
+	NVOroles: "<#942151699718602773>",
+	NVOanythingGoes: "<#938105437180551172>",
+};
+
+let newlyJoinedMemberId = "876850436953481277";
+
 //client obj represents the entire bot:
 
 const client = new Client({
@@ -82,7 +97,7 @@ client.on("messageCreate", (message) => {
 		const number = Math.floor(Math.random() * amount);
 		message.react(list[number]);
 	}
-	//this section is for leaf reacting to leaf. (1079573401888358510 serverleaf) and (1079573678515298344 for NVO/akaServerID#938105437180551168)
+	//this section is for leaf reacting to someone saying "leaf" in their message. (1079573401888358510 serverleaf) and (1079573678515298344 for NVO/akaServerID#938105437180551168)
 	if (message.content.includes("Leaf") || message.content.includes("leaf")) {
 		let id = "";
 		if (message.guild.name === "server") {
@@ -92,24 +107,72 @@ client.on("messageCreate", (message) => {
 		}
 		message.react(id);
 	}
+
+	const embedMessage = {
+		type: "rich",
+		title: `✿﹒welcome  ﹒✿`,
+		description: `Welcome <@${newlyJoinedMemberId}> to NVO,  it's lovely to have you here!\n‎\n﹒verify our ${ChannelTags.NVOrules} & grab some ${ChannelTags.NVOroles}\n\n﹒then come hang out with us here in ${ChannelTags.NVOanythingGoes}!\n‎`,
+		color: 0xedb2aa,
+		image: {
+			url: `https://i.imgur.com/7m2LO4M.png`,
+			height: 0,
+			width: 0,
+		},
+	};
+	if (message.content.includes("test the welcome message embed")) {
+		client.channels.cache
+			.get("962146899220131860")
+			.send({ embeds: [embedMessage] });
+	}
 });
-client.on("messageReactionAdd",async (reaction) =>{
-		//this is for Server
-		if (reaction._emoji.id === "1079573401888358510"){
-			// message.react("1079573401888358510")
-			const channel = await client.channels.fetch(reaction.message.channelId)
-			channel.messages.fetch(reaction.message.id).then(msg=>{
-				msg.react("1079573401888358510")
-			})
-		}
-		//this is for NVO
-				if (reaction._emoji.id === "1079573678515298344"){
-			// message.react("1079573401888358510")
-			const channel = await client.channels.fetch(reaction.message.channelId)
-			channel.messages.fetch(reaction.message.id).then(msg=>{
-				msg.react("1079573678515298344")
-			})
-		}
-	})
+//the following code makes leaf stack his own emoji on any leafemoji
+client.on("messageReactionAdd", async (reaction) => {
+	if (
+		//this is for Server:
+		reaction._emoji.id === "1079573401888358510" ||
+		//this is for NVO:
+		reaction._emoji.id === "1079573678515298344"
+	) {
+		// message.react("1079573401888358510")
+		const channel = await client.channels.fetch(reaction.message.channelId);
+		channel.messages.fetch(reaction.message.id).then((msg) => {
+			msg.react(reaction._emoji.id);
+		});
+	}
+});
+
+//welcome messagre for when new members join in:
+client.on("guildMemberAdd", async (member) => {
+	//when a new member joins happens, get their id and establish all variables we need:
+	newlyJoinedMemberId = member.user.id;
+	//create the embed to send:
+	const embedMessage = {
+		type: "rich",
+		title: `✿﹒welcome  ﹒✿`,
+		description: `Welcome <@${newlyJoinedMemberId}> to NVO,  it's lovely to have you here!\n‎\n﹒verify our ${ChannelTags.NVOrules} & grab some ${ChannelTags.NVOroles}\n\n﹒then come hang out with us here in ${ChannelTags.NVOanythingGoes}!\n‎`,
+		color: 0xedb2aa,
+		image: {
+			url: `https://i.imgur.com/7m2LO4M.png`,
+			height: 0,
+			width: 0,
+		},
+	};
+	//the bot will tag them in a welcome message and mention checking out #rules and #roles! 1078490033037770805
+	const ChannelIDs = {
+		NVOwelcome: "1081977778829795338",
+		NVOtestTestTesting: "962146899220131860",
+		ServerGeneral: "1048059530711404618",
+		DaniDeliaGeneral: "1078490033037770805",
+	};
+	let whereToSend;
+	if (member.guild.id === GuildIDs.DaniDeliaServer) {
+		whereToSend = ChannelIDs.DaniDeliaGeneral;
+	} else if (member.guild.id === GuildIDs.Server) {
+		whereToSend = ChannelIDs.ServerGeneral;
+	} else if (member.guild.id === GuildIDs.NVO) {
+		whereToSend = ChannelIDs.NVOwelcome;
+	}
+	client.channels.cache.get(whereToSend).send({ embeds: [embedMessage] });
+});
 
 client.login(process.env.TOKEN);
